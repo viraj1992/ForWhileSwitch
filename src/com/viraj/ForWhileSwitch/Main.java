a32b58ac45fec32cfb21036d38dc95bce0aee4ff
Viraj Pai <coder.viraj@gmail.com>
2018-01-28 15:12:28 +0530

    Initial commit

--- src/com/viraj/ForWhileSwitch/Main.java
+++ src/com/viraj/ForWhileSwitch/Main.java
@@ -4,39 +4,63 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		// for loop
-		
+
 		System.out.println("--------- for loop -------------");
 		for(int i = 0; i <= 10; i++){
 			System.out.println(i);
 		}
-		
+
 		System.out.println("\n------- while loop -----------");
-		
+
 		// While loop
 		int i = 0;		
 		while (i <= 10) {
 			System.out.println(i);
 			i++;
 		}
-		
+
 		System.out.println("\nThe value of i after while loop is executed : " + i);
-		
+
 		System.out.println("\n------- do while loop --------");
-		
+
 		int y = 40;
-		
+
 		do {
 			System.out.println(y);
 			y++;
 		} while (y <= 15);
-		
+
 		System.out.println("\nThe value of y after do while loop is executed : " + y);
-		
+
 		System.out.println("\n------- switch statement --------");
-	
-		
+
+		String nameString = "Animal Elephant";
+
+		switch(nameString) {
+		case "Animal Name" : 
+			System.out.println("Animal Name");
+			break;
+			
+		case "Animal Cat" :
+			System.out.println("Animal Cat");
+			break;
+			
+		case "Animal Dog" :
+			System.out.println("Animal Dog");
+			break;
+			
+		case "Animal Elephant" :
+			System.out.println("Animal Elephant");
+			break;
+			
+		default :
+			System.out.println("No animal!!!");
+			break;
+
+		}
+
 	}
 
 }
